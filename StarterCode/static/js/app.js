d86921879654f755fcc481673b5547dc516b0d55
Marco Cid <marco.cidm17@gmail.com>
2021-05-02 14:44:54 -0500

    Getting the list

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,6 +1,20 @@
 // Use D3 fetch to read JSON file
 d3.json("data/samples.json").then((importedData) => {
     let data = importedData;
-    
+
+
+    d3.select('#selDataset')
+        .selectAll("option")
+        .data(data.names)
+        .enter()
+        .append("option")
+        .text(d => d)
+
     console.log(data);
 });
+
+d3.select('#selDataset').on("change", updatePanel)
+
+function updatePanel() {
+    
+}
